7f6753eb2d180aeec6a1507dc42593f478320ab9
Aymane Benhima <aymanebenhima@gmail.com>
2021-08-30 10:58:49 +0100

    update block click

---
+++ src/components/Block.vue
@@ -0,0 +1,48 @@
+<template>
+  <div v-if="showBlock" @click="stopTimer" class="block">
+    click me
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['delay'],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true
+      this.startTimer()
+    }, this.delay);
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer)
+      this.$emit('end', this.reactionTime)
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  .block {
+    width: 400px;
+    border-radius: 25px;
+    background: #a29bfe;
+    color: #2d3436;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+  }
+</style>
